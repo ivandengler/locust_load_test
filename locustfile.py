094143087e123c6f8ab151b70c46509881a91099
idengl <ivan.dengler@gmail.com>
2024-05-13 12:23:42 +0300

    Rearranged code and removed unnecessary packages

--- locustfile.py
+++ locustfile.py
@@ -1,22 +1,27 @@
 # Built-in/Generic Imports
-import gevent
-import grpc
+import json
 import random
 import time
-import json
-import grpc_user # type: ignore
+
+import grpc
+from google.protobuf.json_format import MessageToJson
 
 # Libs
 from locust import task
-from users_data import users
-from google.protobuf.json_format import MessageToJson 
+
+import grpc_user  # type: ignore
 
 # Protos
-from proto import auth_service_pb2, auth_service_pb2_grpc
-from proto import vacancy_service_pb2, vacancy_service_pb2_grpc
+from proto import (
+    auth_service_pb2,
+    auth_service_pb2_grpc,
+    vacancy_service_pb2,
+    vacancy_service_pb2_grpc,
+)
+from users_data import users
 
 # Constants
-VACANCIES_URL = 'vacancies.cyrextech.net:7823'
+VACANCIES_URL = "vacancies.cyrextech.net:7823"
 TOTAL_USERS = 3
 TIME_TO_SLEEP_MAIN_LOOP = 30
 TIME_TO_SLEEP_GET_VACANCIES = 45
@@ -43,16 +48,31 @@ class VacanciesLoadTestingUser(grpc_user.GrpcUser):
     def grpc_login_and_return_access_token(self, user):
         with grpc.insecure_channel(VACANCIES_URL) as channel:
             auth_stub = auth_service_pb2_grpc.AuthServiceStub(channel)
-            auth_request = auth_service_pb2.rpc__signin__user__pb2.SignInUserInput(email=user['email'], password=user['password'])
+            auth_request = auth_service_pb2.rpc__signin__user__pb2.SignInUserInput(
+                email=user["email"], password=user["password"]
+            )
             auth_reply = auth_stub.SignInUser(auth_request)
-            return json.loads(MessageToJson(auth_reply))['accessToken']
+            return json.loads(MessageToJson(auth_reply))["accessToken"]
 
     def grpc_create_and_upload_vacancy(self):
-        vacancy_request = vacancy_service_pb2.rpc__create__vacancy__pb2.CreateVacancyRequest(Title='aNewTestTitle ' + str(time.time()), Description='aNewTestDescription ' + str(time.time()), Country="ARG", Division=0)
-        return json.loads(MessageToJson(self.stub.CreateVacancy(vacancy_request)))['vacancy']['Id']
+        vacancy_request = (
+            vacancy_service_pb2.rpc__create__vacancy__pb2.CreateVacancyRequest(
+                Title="aNewTestTitle " + str(time.time()),
+                Description="aNewTestDescription " + str(time.time()),
+                Country="ARG",
+                Division=0,
+            )
+        )
+        return json.loads(MessageToJson(self.stub.CreateVacancy(vacancy_request)))[
+            "vacancy"
+        ]["Id"]
 
     def grpc_update_vacancy(self, vacancy_id):
-        vacancy_update_request = vacancy_service_pb2.rpc__update__vacancy__pb2.UpdateVacancyRequest(Id=vacancy_id, Title='Awesome new title ' + str(time.time()))
+        vacancy_update_request = (
+            vacancy_service_pb2.rpc__update__vacancy__pb2.UpdateVacancyRequest(
+                Id=vacancy_id, Title="Awesome new title " + str(time.time())
+            )
+        )
         return self.stub.UpdateVacancy(vacancy_update_request)
 
     def grpc_get_vacancy(self, vacancy_id):
@@ -66,5 +86,4 @@ class VacanciesLoadTestingUser(grpc_user.GrpcUser):
     def on_start(self):
         user = users[random.randint(0, TOTAL_USERS - 1)]
         self.grpc_login_and_return_access_token(user)
-        #gevent.spawn(self._on_background())
-            
+        # gevent.spawn(self._on_background())
